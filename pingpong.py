391f80e8b028f23ba95f2d5ebf96468faef5f67a
vas008 <102179609+vas008@users.noreply.github.com>
2022-03-29 21:34:36 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -11,6 +11,34 @@ class GameSprite(sprite.Sprite):
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+        
+class Player(GameSprite):
+    def update1(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 140:
+            self.rect.y += self.speed
+    def update2(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 140:
+            self.rect.y += self.speed
+
+class Ball(GameSprite):
+    def update(self):
+        pass
+
+win_width = 800
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption("Пинг-понг")
+window.fill((200, 150, 230))
+
+player1 = Player('racket.png', 50, 180, 5, 40, 140)
+player2 = Player('racket.png', 710, 180, 5, 40, 140)
+ball = Ball('tenis_ball.png', 375, 225, 5, 50, 50)
 
 game = True
 finish = False
